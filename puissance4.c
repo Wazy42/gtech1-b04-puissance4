89527adbd71b5b430d9460ae9509bded87a9159e
Lucas <lvocanson@gaming.tech>
2021-10-15 13:28:45 +0200

    Squelette puissance 4

--- puissance4.c
+++ puissance4.c
@@ -1,6 +1,17 @@
-#include <stdio.h> //MAKE_THE_PROJECT
-int main() {
-  printf('Hello');
-  return 0;
+#include <stdio.h>
+int jeu() {
+  int turn, player;
+  int table[7][6];
+  for (turn=0; turn<21; turn++) {
+    for (player=0; player=0; player++) {
+      // Afficher tableau
+      // Demander colonne
+      // Mettre à jour tableau
+      // Vérfier la victoire
+    }
+  }
 }
-//MAIN
+
+int main() {
+  jeu();
+}
